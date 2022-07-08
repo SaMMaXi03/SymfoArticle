a84058f4061f8c79c533fc1f0afe497a17fa45be
SaMMaXi03 <maximedevaux93@gmail.com>
2022-07-08 11:53:21 +0200

    adminMaster2

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -6,7 +6,6 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
-use ContainerSqmjNoM\getCategoryRepositoryService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
